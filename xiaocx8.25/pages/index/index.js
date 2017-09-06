e00aa91574e31d0f979edf9c57e5a4df23231853
Lucy20209060 <you@example.com>
2017-09-06 13:52:34 +0800

    lu

---
+++ xiaocx8.25/pages/index/index.js
@@ -0,0 +1,66 @@
+//index.js
+//获取应用实例
+
+import { HOME_API } from '../../utils/api'
+console.log(HOME_API)
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    site:[]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    // console.log('跳转到logs页面')
+    // 跳转
+    wx.navigateTo({
+      url: '../news/news'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      console.log(userInfo)
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  // 页面滚动
+  onPageScroll:function(){
+    console.log(1111)
+    this.setData({
+      motto: 'luchao'
+    })
+  },
+  tapTest:function(){
+    this.setData({
+      motto:'lucy'
+    });
+
+    const that = this;
+
+    const getlist = wx.request({
+      url: HOME_API.getlist,
+      data: {},
+      success: function (res) {
+
+        that.setData({
+          site:res.data.data
+        })
+
+        console.log(that.data.site)
+
+      }
+    })
+    // 取消请求任务
+    // getlist.abort();
+  }
+})
+
+
+
---
+++ xiaocx8.25/utils/api.js
@@ -0,0 +1,12 @@
+const apiPre = "http://b2b.anchumall.com";
+// const API = {
+//   index:{
+//     getlist: `${apiPre}/api.php?s=api/site/getlist`
+//   }
+// }
+// export default API;
+
+
+export const HOME_API = {
+  getlist: `${apiPre}/api.php?s=api/site/getlist`
+}
