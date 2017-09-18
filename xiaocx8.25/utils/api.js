155f46e80c52936669e975e2b705a8b6fd59f72c
Lucy20209060 <you@example.com>
2017-09-18 18:30:42 +0800

    xiaocx9.6

--- xiaocx8.25/utils/api.js
+++ xiaocx8.25/utils/api.js
@@ -1,11 +1,4 @@
 const apiPre = "http://b2b.anchumall.com";
-// const API = {
-//   index:{
-//     getlist: `${apiPre}/api.php?s=api/site/getlist`
-//   }
-// }
-// export default API;
-
 
 export const HOME_API = {
   getlist: `${apiPre}/api.php?s=api/site/getlist`
